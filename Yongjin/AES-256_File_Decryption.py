a8509e8c9316711fa407c988a7a9ae8d7148fb2a
YongJin04 <144215228+YongJin04@users.noreply.github.com>
2023-10-05 11:37:07 +0900

    Update AES-256_File_Decryption.py

--- Yongjin/AES-256_File_Decryption.py
+++ Yongjin/AES-256_File_Decryption.py
@@ -8,17 +8,22 @@ def decrypt_aes_ctr(encrypted_data, key, iv):
     # Create a counter object for AES-CTR mode with a 128-bit block size.
     ctr = Counter.new(128, initial_value=int.from_bytes(iv, byteorder='big'))
     cipher = AES.new(key, AES.MODE_CTR, counter=ctr)
+    
+    # Decrypt the encrypted_data using AES-CTR mode.
     decrypted_data = cipher.decrypt(encrypted_data)
+    
     return decrypted_data
 
 if __name__ == "__main__":
-    # Input AES key and IV as hexadecimal strings.
-    aes_key_hex = input("AES Encryption Key (Hex): ").strip()
-    iv_hex = input("IV (Hex): ").strip()
+    # Input the AES key as a 48-byte hexadecimal string.
+    key_hex = input("AES Key (48 bytes in hex): ").strip()
 
-    # Convert hexadecimal strings to bytes.
-    aes_key = bytes.fromhex(aes_key_hex)
-    iv = bytes.fromhex(iv_hex)
+    # Convert the hexadecimal string to bytes.
+    key_bytes = bytes.fromhex(key_hex)
+
+    # Extract the first 32 bytes as the encryption key and the next 16 bytes as the IV.
+    aes_key = key_bytes[:32]
+    iv = key_bytes[32:]
 
     # Specify the input folder path.
     folder_path = 'C:/Users/kyj08/OneDrive/바탕 화면/Test/'  # Enter the desired folder path here.
@@ -38,6 +43,8 @@ if __name__ == "__main__":
     # Define the byte_size to read from the beginning of each file.
     byte_size = 256  # Fixed byte size
     
+    decryption_successful = True  # Flag to track decryption success
+
     for file_path in file_list:
         try:
             with open(file_path, 'rb+') as file:
@@ -53,7 +60,13 @@ if __name__ == "__main__":
 
                 # Print the results.
                 print(f"File partially decrypted: {file_path}")
-                print("AES-CTR Decrypted Hex value:", binascii.hexlify(decrypted_data).decode('utf-8').upper())
+                # print("AES-CTR Decrypted Hex value:", binascii.hexlify(decrypted_data).decode('utf-8').upper())
 
         except Exception as e:
             print(f"An error occurred for file {file_path}: {e}")
+            decryption_successful = False
+
+    if decryption_successful:
+        print("Decryption Successful!")  # Print a success message if all files are successfully decrypted
+    else:
+        print("Decryption Failed for some files!")  # Print a failure message if decryption failed for some files
