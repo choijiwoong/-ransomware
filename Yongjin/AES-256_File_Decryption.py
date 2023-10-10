d074927fdb7a6abd2bb6e41b62526d1ca25a2fed
Yongjin <144215228+YongJin04@users.noreply.github.com>
2023-10-10 16:13:17 +0900

    Update AES-256_File_Decryption.py

--- Yongjin/AES-256_File_Decryption.py
+++ Yongjin/AES-256_File_Decryption.py
@@ -1,72 +1,68 @@
 from Crypto.Cipher import AES
 from Crypto.Util import Counter
 import os
-import binascii
 
 # AES decryption function
-def decrypt_aes_ctr(encrypted_data, key, iv):
+def decrypt_aes_ctr(encrypted_data, aes_key, iv):
     # Create a counter object for AES-CTR mode with a 128-bit block size.
     ctr = Counter.new(128, initial_value=int.from_bytes(iv, byteorder='big'))
-    cipher = AES.new(key, AES.MODE_CTR, counter=ctr)
-    
-    # Decrypt the encrypted_data using AES-CTR mode.
+    cipher = AES.new(aes_key, AES.MODE_CTR, counter=ctr)
     decrypted_data = cipher.decrypt(encrypted_data)
-    
     return decrypted_data
 
-if __name__ == "__main__":
-    # Input the AES key as a 48-byte hexadecimal string.
-    key_hex = input("AES Key (48 bytes in hex): ").strip()
+# Decrypt a file using AES-256 CTR mode.
+def decrypt_file(file_path, key, IV):
+    try:
+        with open(file_path, 'rb+') as file:
+            # Define the byte_size to read from the beginning of each file.
+            byte_size = 256  # Fixed byte size
 
-    # Convert the hexadecimal string to bytes.
-    key_bytes = bytes.fromhex(key_hex)
+            binary_data = file.read(byte_size)
+            decrypted_data = decrypt_aes_ctr(binary_data, key, IV)
+            file.seek(0)
+            file.write(decrypted_data)
+            return True
+    except Exception as e:
+        # Do not print the error message and file path.
+        return False
+
+# Decrypt all files in a folder and its subfolders.
+def decrypt_files_in_folder(decrypt_folder_path, key, IV):
+    file_list = []
+    extensions = ('zip', 'hwp', 'hwpx', 'xlsx', 'docx', 'pptx', 'pdf')
+
+    decryption_successful = True
+
+    for root, dirs, files in os.walk(decrypt_folder_path):
+        for file in files:
+            ext = file.lower().split('.')[-1]
+            if ext in extensions:
+                file_path = os.path.join(root, file)
+                success = decrypt_file(file_path, key, IV)
+                if not success:
+                    decryption_successful = False
+
+    print_decryption_result(decryption_successful)
+
+# Print success or failure of decryption.
+def print_decryption_result(successful):
+    if successful:
+        print("Decryption Successful!")
+    else:
+        print("Decryption Failed for some files!")
+
+if __name__ == "__main__":
+    # Input AES key as a 48-byte hexadecimal string.
+    combine_key_hex = input("AES Key (48 bytes in hex): ").strip()
+
+    # Convert hexadecimal string to bytes.
+    combine_key_bytes = bytes.fromhex(combine_key_hex)
 
     # Extract the first 32 bytes as the encryption key and the next 16 bytes as the IV.
-    aes_key = key_bytes[:32]
-    iv = key_bytes[32:]
+    key = combine_key_bytes[:32]
+    IV = combine_key_bytes[32:]
 
-    # Specify the input folder path.
-    folder_path = 'C:/Users/kyj08/OneDrive/바탕 화면/Test/'  # Enter the desired folder path here.
+    # Specify the input folder path for decryption.
+    decrypt_folder_path = 'C:/'  # Enter the desired folder path here.
 
-    # Create an empty list to store the file paths of all files in the folder.
-    file_list = []
-
-    # Recursively search for all files in the input folder and its subfolders,
-    # and add the paths of files with the specified extensions to the list.
-    extensions = ('zip', 'hwp', 'hwpx', 'xlsx', 'docx', 'pptx', 'pdf')  # Specify the extensions to search for
-    for root, dirs, files in os.walk(folder_path):
-        for file in files:
-            ext = file.lower().split('.')[-1]  # Get the file extension
-            if ext in extensions:
-                file_list.append(os.path.join(root, file))
-
-    # Define the byte_size to read from the beginning of each file.
-    byte_size = 256  # Fixed byte size
-    
-    decryption_successful = True  # Flag to track decryption success
-
-    for file_path in file_list:
-        try:
-            with open(file_path, 'rb+') as file:
-                # Read the first byte_size bytes of the file as binary data.
-                binary_data = file.read(byte_size)
-
-                # Decrypt the binary_data using AES-256 CTR mode with the provided key and IV.
-                decrypted_data = decrypt_aes_ctr(binary_data, aes_key, iv)
-
-                # Seek back to the beginning of the file and write the decrypted_data.
-                file.seek(0)
-                file.write(decrypted_data)
-
-                # Print the results.
-                print(f"File partially decrypted: {file_path}")
-                # print("AES-CTR Decrypted Hex value:", binascii.hexlify(decrypted_data).decode('utf-8').upper())
-
-        except Exception as e:
-            print(f"An error occurred for file {file_path}: {e}")
-            decryption_successful = False
-
-    if decryption_successful:
-        print("Decryption Successful!")  # Print a success message if all files are successfully decrypted
-    else:
-        print("Decryption Failed for some files!")  # Print a failure message if decryption failed for some files
+    decrypt_files_in_folder(decrypt_folder_path, key, IV)
